6be339ee2a8be9ec0a3e910d06504a36952acceb
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2023-08-09 16:08:38 +0300

    Bean Validation V23

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -2,12 +2,15 @@ package com.example.application.views.helloworld;
 
 
 import com.example.application.views.*;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.*;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import java.util.Objects;
 
 @PageTitle("Hello World Vaadin 24")
 @Route(value = "")
@@ -19,22 +22,29 @@ public class HelloWorldView extends HorizontalLayout {
     private TextField name = new TextField("Name");
     private TextField name2 = new TextField("Name2");
 
+    private Span errorMessage = new Span();
+
     public HelloWorldView() {
         FakeBean fakeBean = new FakeBean();
         binder.forField(name).asRequired("name is required")
-                .withValidator(value -> {
-                    System.out.println("Validate " + value);
-                    return (value.length() > 2);
-                }, "Length should be > 2")
                 .bind(FakeBean::getName, FakeBean::setName);
         binder.forField(name2).asRequired("name2 is required")
-                .withValidator(value -> {
-                    System.out.println("Validate name2 " + value);
-                    return (value.length() > 2);
-                }, "Length should be > 2")
                 .bind(FakeBean::getName2, FakeBean::setName2);
         binder.setBean(fakeBean);
-        add(name, name2);
+        binder.withValidator(bean ->
+                        Objects.equals(bean.getName(), "test") || Objects.equals(bean.getName2(), "test"),
+                "At least one field should be test"
+        );
+        binder.setValidationStatusHandler(status -> {
+            System.out.println("Errors " + status.getValidationErrors().size());
+            if (!status.getBeanValidationErrors().isEmpty()) {
+                errorMessage.setText(status.getBeanValidationErrors().get(0).getErrorMessage());
+                errorMessage.setVisible(true);
+            } else {
+                errorMessage.setVisible(false);
+            }
+        });
+        add(name, name2, errorMessage);
     }
 
 }
